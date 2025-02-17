2642b610924612f2cfcf375b790686c3d570f2a0
Kyoung Yeob <kkyub1313@gmail.com>
2025-02-17 20:28:49 +0900

    update(controller): 카트 로직 수정

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -56,6 +56,41 @@ module.exports.addToCart = async (req, res) => {
 };
 
 
+// exports.addToCart = async (req, res) => {
+//     // 참고용, 로직 상 하나가 누락되어 있음.
+
+//     const userId = req.user._id;
+//     const { productId, size, quantity } = req.body;
+
+//     let cart = await Cart.findOne({ user: userId });
+//     if (!cart) {
+//         cart = new Cart({ user: userId, items: [] });
+//     }
+
+//     const product = await Product.findById(productId);
+//     if (!product) {
+//         return res.status(404).json({ message: '상품을 찾을 수 없습니다.' });
+//     }
+
+//     const existingItem = cart.items.find(item => 
+//         item.product.toString() === productId && item.size === size
+//     );
+
+//     if (existingItem) {
+//         existingItem.quantity += quantity;
+//     } else {
+//         cart.items.push({ product: productId, size, quantity });
+//     }
+
+//     await cart.save();
+//     res.json(cart);
+// };
+
+module.exports.getCart = async (req, res) => {
+    const userId = req.user._id;
+    const cart = await Cart.findOne({ user: userId }).populate('items.product');
+    res.json(cart);
+};
 
 
 module.exports.initAddToCart = async (req, res) => {
@@ -100,73 +135,5 @@ module.exports.initAddToCart = async (req, res) => {
 
     await cart.save();
     res.redirect('/cart');
-};
-
-
-
-
-module.exports.index = async (req, res) => {
-    const products = await Product.find({ createdBy: req.user._id });
-    res.render('products/index', { products });
-};
-
-module.exports.renderNewForm = (req, res) => {
-    res.render('products/new');
-};
-
-module.exports.createProduct = async (req, res) => {
-    const product = new Product(req.body.product);
-    product.createdBy = req.user._id;
-    await product.save();
-
-    res.redirect('/products');
-};
-
-module.exports.showProduct = async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    res.render('products/edit', { product });
-};
-
-module.exports.renderEditForm = async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    res.render('products/edit', { product });
-};
-
-module.exports.updateProduct = async (req, res) => {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, { ...req.body.product });
-
-    // 저장한 후 createdBy가 req.user._id인 모든 product를 가져옴
-    const products = await Product.find({ createdBy: req.user._id });
-
-    res.redirect('/products');
-};
-
-
-module.exports.updateQuantity = async (req, res, next) => {
-    const { id } = req.params;
-    const { quantity } = req.body;
-
-    // 유효성 검사
-    if (typeof quantity !== 'number' || quantity < 0 || !Number.isInteger(quantity)) {
-        throw new ExpressError('수량은 0 이상의 정수여야 합니다.', 400);
-    }
-
-    const product = await Product.findById(id);
-    if (!product) {
-        throw new ExpressError('상품을 찾을 수 없습니다.', 404);
-    }
-
-    product.quantity = quantity;
-    await product.save();
-
-    res.json({ success: true, quantity: product.quantity });
-};
-
-
-module.exports.deleteProduct = async (req, res) => {
-    const { id } = req.params;
-    await Product.findByIdAndDelete(id);
-    res.redirect('/products');
 };
 
